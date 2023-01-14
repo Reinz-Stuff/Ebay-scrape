691aba11cec11525af99fa514be0807d56a22188
Fauzi kurniawan <ozilpym123@gmail.com>
2023-01-14 15:57:38 +0700

    done!

--- main.py
+++ main.py
@@ -6,45 +6,57 @@ import pandas as pd
 
 url = 'https://www.ebay.com/sch/i.html?'
 
-params = {
-    '_from': 'R40',
-    '_nkw': 'iphone',
-    '_sacat': '0',
-    '_pgn': '1',
-}
+search = input('product search: ')
 
-req = requests.get(url, params=params)
-soup = BeautifulSoup(req.text, 'html.parser')
 
-table = soup.find('ul', attrs={'class': 'srp-results srp-list clearfix'})
-items = table.find_all('li', attrs={'class': 's-item s-item__pl-on-bottom'})
-
-result = []
-for item in items:
-    title = item.find('div', attrs={'class': 's-item__title'}).text
-    price = item.find('span', attrs={'class': 's-item__price'}).text
-    location = item.find('span', attrs={'class': 's-item__location s-item__itemLocation'}).text
-
-    try:
-        review = item.find('span', attrs={'class': 's-item__reviews-count'}).find('span').text
-    except AttributeError:
-        review = 'No review yet'
-
-    data_dict = {
-        'Product name': title,
-        'Price': price,
-        'Location': location,
-        'Review': review
+def send_req(page):
+    params = {
+        '_from': 'R40',
+        '_nkw': search,
+        '_sacat': '0',
+        '_pgn': page,
     }
-    result.append(data_dict)
 
-try:
-    os.mkdir('results')
-except FileExistsError:
-    pass
+    req = requests.get(url, params=params)
+    soup = BeautifulSoup(req.text, 'html.parser')
 
-df = pd.DataFrame(result)
+    table = soup.find('ul', attrs={'class': 'srp-results srp-list clearfix'})
+    items = table.findAll('li', attrs={'class': 's-item s-item__pl-on-bottom'})
 
-df.to_json('results/iphone.json', orient='records')
-df.to_csv('results/iphone.csv', index=False)
-df.to_excel('results/iphone.xlsx', index=False)
+    result = []
+    for item in items:
+        title = item.find('div', attrs={'class': 's-item__title'}).text
+        price = item.find('span', attrs={'class': 's-item__price'}).text
+        location = item.find('span', attrs={'class': 's-item__location s-item__itemLocation'}).text
+
+        try:
+            review = item.find('span', attrs={'class': 's-item__reviews-count'}).find('span').text
+        except AttributeError:
+            review = 'No review yet'
+
+        data_dict = {
+            'Product name': title,
+            'Price': price,
+            'Location': location,
+            'Review': review
+        }
+        result.append(data_dict)
+    return result
+
+
+def extract_data(res, filename):
+    try:
+        os.mkdir('results')
+    except FileExistsError:
+        pass
+
+    df = pd.DataFrame(res)
+
+    df.to_json(f'results/{filename}.json', orient='records')
+    df.to_csv(f'results/{filename}.csv', index=False)
+    df.to_excel(f'results/{filename}.xlsx', index=False)
+
+
+if __name__ == '__main__':
+    data = send_req(str(1))
+    extract_data(data, search)
